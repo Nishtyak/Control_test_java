1e389b7cce6571eb9c4316841fdef1e168ddfeb1
Tatyana <KrukovaTatyana1990@gmail.com>
2023-12-19 19:40:04 +0300

    Done

--- ToyLottery.java
+++ ToyLottery.java
@@ -1,15 +1,19 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
-/**
- * ToyLottery
- */
+
 public class ToyLottery {
     private int id;
     private String name;
     private int count;
     private double chance;
 
+    /*Функция для записи изменненых данных при выпадении игрушки*/
     private ToyLottery ToyLottery (int id, String name, int count, double chance) {
         this.id = id;
         this.name = name;
@@ -18,6 +22,7 @@ public class ToyLottery {
         return this;
     }
 
+    /*инициализация */
     public ToyLottery (int id, String name, int count, double chance) {
         this.id = id;
         this.name = name;
@@ -25,6 +30,7 @@ public class ToyLottery {
         this.chance = chance;
     }
 
+    /*используемая инициализация для высчитывания шанса выпадения игрушки с добавлением игрушки в коллекцию*/
     public ToyLottery (String name, int count, Set<ToyLottery> toys) {
         this.id = toys.size() + 1;
         this.name = name;
@@ -33,6 +39,7 @@ public class ToyLottery {
         toys.add(ToyLottery(this.id, this.name, this.count, this.chance));
     }
 
+    /*высчитывание шанса выадения игрушки при добавлении игрушек*/
     public int calculateChance(Set<ToyLottery> toys, int count) {
         int sum = count;
         for (ToyLottery toy : toys) {
@@ -43,7 +50,6 @@ public class ToyLottery {
             double result =(double) Math.round((double) toy.getCount() / (double) sum * 100);
             toy.setChance(result);
         }
-
 
         return (int) Math.round((double) count / (double) sum * 100);
     }
@@ -77,7 +83,7 @@ public class ToyLottery {
         return id + " " + name + " кол-во:" + count + " " + chance + "%";
     }
 
-    
+    /*Получение рандомной игрушки*/
     public static String getRandomToy (Set<ToyLottery> toys) {
         Random random = new Random();
         int chance = random.nextInt(1, 100);
@@ -96,6 +102,7 @@ public class ToyLottery {
         return chanceToy.getName();
     }
 
+    /*Изменение шанса выпадения игрушек после выпадения призовой игрушки*/
     private static void changeChances (ToyLottery toy, Set<ToyLottery> toys) {
         toy.setCount(toy.getCount() - 1);
 
@@ -110,4 +117,62 @@ public class ToyLottery {
             toyLottery.setChance(result);
         }
     }
+
+    /*проведение лотереи*/
+    public static List<String> haveLottery (int lotteryNum, Set<ToyLottery> toys) {
+        List<String> prizes = new ArrayList<>();
+
+        for (int i = 0; i < lotteryNum; i++) {
+            prizes.add(ToyLottery.getRandomToy(toys));
+        }
+
+        return prizes;
+    }
+
+    /*Получить приз из выпавших игрушек*/
+    public static void getPrize (List<String> prizes) {
+        System.out.println();
+        if(prizes.size() > 0) {
+            String prize = prizes.remove(0);
+            try(FileWriter fileWriter = new FileWriter("receivedPrizes.txt", true);) {
+                fileWriter.write(prize + "\n");
+                System.out.println("Ваш приз: " + prize);
+            } catch(Exception e) {
+                System.out.println("Ошибка!");
+            }
+        } else {
+            System.out.println("Призы закончились");
+        }
+        System.out.println();
+    }
+
+    /*Основная функция лотереи*/
+    public static void lottery (Set<ToyLottery> toys) {
+
+        System.out.print("Введите количество розыгрышей: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        List<String> prizes = ToyLottery.haveLottery(num, toys);
+        System.out.println("Лотерея завершена");
+        System.out.println("Выпавшие игрушки: ");
+        for (String prize : prizes) {
+            System.out.print(prize + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        while (true) {
+            System.out.println("Чтобы выдать игрушку, нажмите 1" + "\n"+
+                                "Для вызода нажмите 0");
+            System.out.print("Введите команду: ");
+            int command = input.nextInt();
+            if (command == 0) {
+                break;
+            } else if (command == 1) {
+                ToyLottery.getPrize(prizes);
+            }
+        }
+    }
+
 }
--- program.java
+++ program.java
@@ -4,24 +4,14 @@ import java.util.Set;
 public class program {
 
     public static void main(String[] args) {
+
         Set<ToyLottery> toys = new HashSet<>();
         ToyLottery toy1 = new ToyLottery("мишка", 20, toys);
-        // toys.add(toy1);
         ToyLottery toy2 = new ToyLottery("зайчик", 10, toys);
         ToyLottery toy3 = new ToyLottery("котик", 10, toys);
         ToyLottery toy4 = new ToyLottery("пингвин", 15, toys);
 
-        // for (ToyLottery toy : toys) {
-        //     System.out.println(toy.toString());
-        // }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(ToyLottery.getRandomToy(toys));
-            // for (ToyLottery toy : toys) {
-            //     System.out.println(toy.toString());
-            // // }
-            // System.out.println();
-        }
-
+        /*Вызов функции лотереи*/
+        ToyLottery.lottery(toys);
     }
 }
